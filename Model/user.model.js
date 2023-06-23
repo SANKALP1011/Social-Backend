8172137a488709d2eb8454478b89ad1472d7f9f9
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-06-24 01:58:53 +0530

    dome with api

--- Model/user.model.js
+++ Model/user.model.js
@@ -13,38 +13,6 @@ const UserModel = new moongose.Schema({
     type: String,
     default: "",
   },
-  facebookId: {
-    type: String,
-    default: "",
-  },
-  twitterId: {
-    type: String,
-    default: "",
-  },
-  FaceBookDisplayName: {
-    type: String,
-    default: "",
-  },
-  TwitterDisplayName: {
-    type: String,
-    default: "",
-  },
-  TwitterLocation: {
-    type: "String",
-    default: "",
-  },
-  TwitterDiscription: {
-    type: "String",
-    default: "",
-  },
-  TwitterFriendsCount: {
-    type: Number,
-    default: 0,
-  },
-  TwitterFollowerCount: {
-    type: Number,
-    default: 0,
-  },
 });
 
 module.exports = moongose.model("user", UserModel);
